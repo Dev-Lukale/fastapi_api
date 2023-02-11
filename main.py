ff79802e99549b798acb845c2438c73e35b0992e
LukaleTyson <lukaletyson99@gmail.com>
2023-02-11 18:57:07 +0300

    On branch verify_email_feature Changes to be committed: 	modified:   authUtil.py 	modified:   database.sqlite3-shm 	modified:   database.sqlite3-wal 	new file:   mail.py 	modified:   main.py 	new file:   templates/verification.html

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request,HTTPException,status
 from tortoise.contrib.fastapi import register_tortoise
 from tortoise import models
-from models import *
+from models import (User, Business, Product, user_pydantic, user_pydanticIn, 
+                    product_pydantic,product_pydanticIn, business_pydantic, 
+                    business_pydanticIn, user_pydanticOut)
 from authUtil import *
+from mail import *
 
 #signals
 from tortoise.signals import post_save
 from typing import List,Optional,Type
 from tortoise import BaseDBAsyncClient
+
+from fastapi.responses import HTMLResponse
+
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
@@ -26,6 +33,7 @@ async def create_business(
         )
         await business_pydantic.from_tortoise_orm(business_obj)
         #send the email
+        await send_email([instance.email],instance)
 @app.get("/")
 def index():
     return{"Message":"Welcome User"}
@@ -41,6 +49,21 @@ async def register(user:user_pydanticIn):
         "status":"OK",
         "data":f"Hello {new_user.username},Please check your email inbox to verify your registration"
     }
+
+
+templates=Jinja2Templates(directory="templates")
+@app.get("/verification",response_class=HTMLResponse)
+async def verify_email(request: Request,token:str):
+    user=await verify_token(token)
+    if user and not user.is_verified:
+        user.is_verified=True
+        await user.save()
+        return templates.TemplateResponse("Verification.html",{"request": request,"username":user.username})
+    raise HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Invalid token",
+    headers={"WWW.Authenticate":"Bearer"}
+    )
 
 register_tortoise(
     app,
